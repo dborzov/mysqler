21dd0e23228622ada5b604944c0cba6d665d3fb9
Peter Borzov <peter@sowingo.com>
2014-06-04 11:55:16 -0400

    Address parameter flag

--- overseer.go
+++ overseer.go
@@ -2,30 +2,45 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var MySQLAddress = flag.String("address", "root@tcp(localhost:3306)/peter", "Database connection address")
+
 func main() {
-	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/peter")
+	flag.Parse()
+	db, err := sql.Open("mysql", *MySQLAddress)
 	if err != nil {
+		fmt.Println("Unable to connect to that address, baby")
+		fmt.Println("-----------------------------------")
 		panic(err.Error())
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT name, is_active FROM user")
+	rows, err := db.Query(`SELECT 
+								product.id, 
+								product.name,
+								product.category_id,
+								category.name
+							FROM 
+							    product,
+							    category
+							WHERE
+							    product.category_id=category.id;`)
+
 	if err != nil {
 		panic(err.Error())
 	}
 
 	for rows.Next() {
-		var name string
-		var active bool
-		if err := rows.Scan(&name, &active); err != nil {
+		var id, name, category_id, category_name string
+		if err := rows.Scan(&id, &name, &category_id, &category_name); err != nil {
 			panic(err.Error())
 		}
 		fmt.Println("-----------------------------------")
 		fmt.Println("Name: ", name)
-		fmt.Println("Active: ", active)
+		fmt.Println("Category: ", category_name)
 	}
 }
